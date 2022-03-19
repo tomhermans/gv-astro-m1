ce316bc0eb372b3db6ea09577fae9ae9c7888e13
Tom Hermans <tomhermans@gmail.com>
2022-03-19 14:31:26 +0100

    first commit

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,18 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+<div id="svelte" class="counter">
+  <p>Svelte counter</p>
+  <button on:click={subtract}>-</button>
+  <pre>{count}</pre>
+  <button on:click={add}>+</button>
+</div>
