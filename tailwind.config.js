a62e3b9206aae475f96cdd68270039174f68e927
Tom Hermans <tomhermans@gmail.com>
2022-04-06 18:26:52 +0200

    import new block components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,35 @@ module.exports = {
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
   // ...other options come here
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          50: "#FCF3F0",
+          100: "#f0e4e0",
+          200: "#dcc5bc",
+          300: "#c9a99c",
+          400: "#b38c7a",
+          500: "#9c735e",
+          600: "#7a5d4d",
+          700: "#564438",
+          800: "#372c25",
+          900: "#15110f",
+        },
+        gvgreen: {
+          100: "#ecf3e7",
+          200: "#cee3c0",
+          300: "#b2d59a",
+          400: "#91c86f",
+          500: "#72be46",
+          600: "#599e33",
+          700: "#3e7722",
+          800: "#285115",
+          900: "#112509",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 };
