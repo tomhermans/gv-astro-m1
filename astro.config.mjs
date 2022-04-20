28b83a9bec4be82504acf8fb8ca7b4648fdef0d8
Tom Hermans <tomhermans@gmail.com>
2022-04-20 18:25:59 +0200

    change astro.request to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,47 @@
-import { imagetools } from "vite-imagetools";
+// import { imagetools } from "vite-imagetools";
 
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: "http://example.com", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  vite: {
-    define: {
-      // "process.env.​VITE_BUILD_TIME": JSON.stringify(newDate().toISOString()),
-    },
-    plugins: [imagetools({ force: true })],
-  },
-  devOptions: {
-    tailwindConfig: "./tailwind.config.js", // Path to Tailwind config file.
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: ["@astrojs/renderer-svelte"],
-};
+// export default {
+//   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+//   // pages: './src/pages', // Path to Astro components, pages, and data
+//   // dist: './dist',       // When running `astro build`, path to final static output
+//   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+//   buildOptions: {
+//     site: "http://example.com", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+//     sitemap: true, // Generate sitemap (set to "false" to disable)
+//   },
+//   vite: {
+//     define: {
+//       // "process.env.​VITE_BUILD_TIME": JSON.stringify(newDate().toISOString()),
+//     },
+//     plugins: [imagetools({ force: true })],
+//   },
+//   devOptions: {
+//     tailwindConfig: "./tailwind.config.js", // Path to Tailwind config file.
+//     // hostname: 'localhost',  // The hostname to run the dev server on.
+//     // port: 3000,             // The port to run the dev server on.
+//   },
+//   renderers: ["@astrojs/renderer-svelte"],
+// };
+
+// import { imagetools } from "vite-imagetools";
+import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+// import tailwindConfig from "./tailwind.config.js";
+
+// https://astro.build/config
+export default defineConfig({
+  server: { port: 1234, host: true },
+  // devOptions: {
+  //   tailwindConfig: "./tailwind.config.js",
+  // },
+  integrations: [
+    // tailwind(),
+    tailwind({
+      config: {
+        applyBaseStyles: true,
+      },
+    }),
+    svelte(),
+  ],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,34 @@ module.exports = {
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
   // ...other options come here
+  theme: {
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    // fontFamily: {
+    //   sans: ["Graphik", "sans-serif"],
+    //   serif: ["Merriweather", "serif"],
+    // },
+    extend: {
+      colors: {
+        "gray-dark": "#273444",
+        "green-blue": "#7dd3fc",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
 };
